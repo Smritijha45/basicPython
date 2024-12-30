db44ed1222999b8a6791460fa5c44c55b7cd20a0
Smriti Jha <smritijha5487@gmail.com>
2024-12-31 03:25:50 +0530

    basicpython

---
+++ one.py
@@ -0,0 +1,57 @@
+# name = "smriti"
+# age = 19
+# price = 55.66
+# print("My age is:", age)
+# print(type(age))
+# ARTHIMETIC
+# a=5
+# b=2
+# print(a+b)
+# print(a-b)
+# print(a*b)
+# print(a/b)
+# print(a%b)
+# print(a**b)
+# RELATIONAL
+# a=50
+# b=60
+# print(a==b)
+# print(a!=b)
+# print(a>=b)
+# print(a<=b)
+# print(a<b)
+# print(a>b)
+# LOGICAL
+# print(not True)
+# print(not False)
+# val1=True
+# val2=False
+# print(val1 and val2)
+# print(val1 or val2)
+# Type conversion
+# a=5
+# b=5.6
+# print(a+b)
+# Type casting
+# a=5
+# b=float("6.6")
+# print(a+b)
+# question1
+# num1=input("Enter firts num:")
+# num2=input("Enter second num:")
+# sum=num1+num2
+# print("The sum is:",sum)
+# question2
+# s1=float(input("Enter side one:"))
+# s2=float(input("Enter side two:"))
+# ar=s1*s2
+# print("Area of the sqaure is:",ar)
+# question3
+# n1=float(input("Enter one no:"))
+# n2=float(input("Enter two no:"))
+# avg=(n1+n2)/2
+# print("The avg is:",avg)
+# question4
+# n1=int(input("Enter one no:"))
+# n2=int(input("Enter two no:"))
+# print(n1>n2)
